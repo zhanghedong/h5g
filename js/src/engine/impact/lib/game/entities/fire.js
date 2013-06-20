4deeb735584df7822a2609a7fdc32f1b3338991f
1625 <263135>
2013-06-20 22:59:55 +0800

    xx

--- js/src/engine/impact/lib/game/entities/fire.js
+++ js/src/engine/impact/lib/game/entities/fire.js
@@ -1,16 +1,16 @@
 ig.module(
-        'game.entities.shaizi'
+        'game.entities.fire'
     )
     .requires(
         'impact.entity'
     )
     .defines(function () {
 
-        EntityShaizi = ig.Entity.extend({
+        EntityFire = ig.Entity.extend({
 
             // The players (collision) size is a bit smaller than the animation
             // frames, so we have to move the collision box a bit (offset)
-            size: {x: 15, y: 17},
+            size: {x: 100, y: 96},
             offset: {x: 4, y: 2},
             _wmScalable:true,
             maxVel: {x: 100, y: 200},
@@ -20,7 +20,7 @@ ig.module(
             checkAgainst: ig.Entity.TYPE.NONE,
             collides: ig.Entity.COLLIDES.PASSIVE,
 
-            animSheet: new ig.AnimationSheet('resources/images/anmi/shaizi.png', 90, 104),
+            animSheet: new ig.AnimationSheet('resources/images/anmi/xxx.png', 100, 96),
 
 
             // These are our own properties. They are not defined in the base
@@ -36,9 +36,10 @@ ig.module(
                 this.parent(x, y, settings);
                 var image = this.animSheet.image;
                 console.log(image);
+//                this.accel.x = -90;
 //                image.resize(1.1);
                 // Add the animations
-                this.addAnim('idle', 0.2, [0,1,2,3,4,5]);
+                this.addAnim('idle', 0.2, [0,1,2,3,4],true);
                // this.addAnim('run', 0.07, [0, 1, 2, 3, 4, 5]);
                // this.addAnim('jump', 1, [9]);
                // this.addAnim('fall', 0.4, [6, 7]);
